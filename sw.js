723ed5798fe3f45226780f7a48d7ade6250a5e08
BenceTuri <benceturi2@gmail.com>
2022-09-27 13:34:14 +0200

    Tons of changes!
    I added a ton of changes and features:
    -Light mode(I needs a little bit more work)
    -Better PWA compatibility
    -Menu for theme, fullscreen and about page
    -About page

---
+++ sw.js
@@ -0,0 +1,23 @@
+self.addEventListener("install", (e) => {
+  e.waitUntil(
+    caches.open("static").then((cache) => {
+      return cache.addAll([
+        "./",
+        "./style.css",
+        "./icons/logo192.png",
+        "./info.html",
+        "./index.html",
+        "./sw.js",
+        "./favicon.png",
+      ]);
+    })
+  );
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
+    })
+  );
+});
